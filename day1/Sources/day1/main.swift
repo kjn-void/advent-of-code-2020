00118fa3a5589a05b82233cb1467644819d2fe01
Kenneth Jonsson <kenneth.jonsson@windriver.com>
2020-12-01 07:00:14 +0000

    Binary search

--- day1/Sources/day1/main.swift
+++ day1/Sources/day1/main.swift
@@ -1,33 +1,49 @@
 import Foundation
 
-func inputGet() -> [UInt64] {
+func inputGet() -> [Int64] {
     let cwdURL = URL(fileURLWithPath: FileManager.default.currentDirectoryPath)
     let fileURL = URL(fileURLWithPath: "input", relativeTo: cwdURL).appendingPathExtension("txt")
     let content = try! String(contentsOf: fileURL, encoding: String.Encoding.utf8)
-    return content.components(separatedBy: "\n").filter { $0.length > 0 }.map { UInt64($0)! }
+    return content.components(separatedBy: "\n").filter { $0.length > 0 }.map { Int64($0)! }
 }
 
-let input = inputGet()
-let requiredSum = 2020
+let input = inputGet().sorted { $0 < $1 } 
+let requiredSum: Int64 = 2020
 
-func part1(expenseReport: [UInt64]) -> UInt64? {
-    for (i, x) in input.enumerated() {
-        for y in input[(i+1)...] {
-            if x + y == requiredSum {
-                return x * y
+extension RandomAccessCollection {    
+    func binarySearch(predicate: (Element) -> Int64) -> Element? {
+        var lo = startIndex
+        var hi = endIndex
+        while lo != hi {
+            let i = index(lo, offsetBy: distance(from: lo, to: hi) / 2)
+            switch predicate(self[i]) {
+            case let diff where diff < 0:
+                lo = index(after: i)
+            case let diff where diff > 0:
+                hi = i
+            default:
+                return self[i]
             }
+
+        }
+        return nil
+    }
+}
+
+func part1(expenseReport: [Int64]) -> Int64? {
+    for (i, x) in input.enumerated() {
+        if let y = input[(i+1)...].binarySearch(predicate: { $0 + x - requiredSum }) {
+            return x * y
         }
     }
     return nil
 }
 
-func part2(expenseReport: [UInt64]) -> UInt64?  {
+func part2(expenseReport: [Int64]) -> Int64?  {
     for (i, x) in input.enumerated() {
         for (j, y) in input[(i+1)...].enumerated() {
-            for z in input[(j+1)...] {
-                if x + y + z == requiredSum {
-                    return x * y * z
-                }
+            if let z = input[(j+1)...].binarySearch(predicate: { $0 + x + y - requiredSum }) {
+                return x * y * z
             }
         }
     }
