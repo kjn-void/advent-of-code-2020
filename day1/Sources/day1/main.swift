0cd3326077042b02c0f7f7dd5ddd959294db7c4f
Kenneth Jonsson <kenneth.jonsson@windriver.com>
2020-12-01 12:59:50 +0000

    Added tests for day1

--- day1/Sources/day1/main.swift
+++ day1/Sources/day1/main.swift
@@ -1,8 +1,9 @@
 import Foundation
 
 func inputGet() -> [UInt64] {
+    let fileName = CommandLine.argc == 1 ? "input" : CommandLine.arguments[1]
     let cwdURL = URL(fileURLWithPath: FileManager.default.currentDirectoryPath)
-    let fileURL = URL(fileURLWithPath: "input", relativeTo: cwdURL).appendingPathExtension("txt")
+    let fileURL = URL(fileURLWithPath: fileName, relativeTo: cwdURL).appendingPathExtension("txt")
     let content = try! String(contentsOf: fileURL, encoding: String.Encoding.utf8)
     return content.components(separatedBy: "\n").filter { $0.length > 0 }.map { UInt64($0)! }
 }
